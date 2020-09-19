1d08773e82fcd04001a0fd0f67e2afdc65ea4179
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-09-19 17:21:40 -0500

    trends

--- src/services/getGifs.jsx
+++ src/services/getGifs.jsx
@@ -1,16 +1,20 @@
-const apiKey = password;
-export default function getGifs({keyword = 'apple'} = {}) {
-    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=5&offset=0&rating=g&lang=en`;
-    return fetch(apiURL)
-    .then(res => res.json())
-    .then(response => {
-        const { data } = response;
+import {API_KEY, API_URL} from './settings';
+const fromApiResponseToGifs = apiResponse => {
+    const {data = []} = apiResponse;
+    if(Array.isArray(data)) {
         const gifs = data.map(image => {
             const {images, title, id} = image;
             const {url} = images.downsized_medium;
-            return {title, id, url}
-        });
+            return {title, id, url};
+        })
         return gifs;
-    });
+    }
+    return [];
 }
 
+export default function getGifs({Limit = 10, keyword = 'spiderman'} = {}) {
+    const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${Limit}&offset=0&rating=g&lang=en`;
+    return fetch(apiURL)
+        .then(res => res.json())
+        .then(fromApiResponseToGifs);
+}  
